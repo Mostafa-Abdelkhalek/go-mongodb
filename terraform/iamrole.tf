2eaed90e2dc8b18911143bd110b4520083235b17
Mostafa-Abdelkhalek <mostafaabdelkhlk8@gmail.com>
2024-01-25 20:08:54 +0200

    last

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -21,8 +21,7 @@ resource "aws_iam_role_policy_attachment" "role1-policy-AmazonEKSClusterPolicy" 
   role       = aws_iam_role.eks-role.name
 }
 
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+
 resource "aws_iam_role_policy_attachment" "role2-policy-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks-role.name
